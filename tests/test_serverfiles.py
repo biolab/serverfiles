6a0a235a2b5006372a7a0d01d851e33dfdf027c9
Marko Toplak <marko.toplak@gmail.com>
2016-10-11 16:51:35 +0200

    More tests.

--- tests/test_serverfiles.py
+++ tests/test_serverfiles.py
@@ -3,25 +3,47 @@
 
 import unittest
 import multiprocessing
-import time
 import os
 import shutil
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import tempfile
+import gzip
+import bz2
+import tarfile
 
 import serverfiles
 
 
-DATETIMETEST = "HERE"
+DATETIMETEST = "2013-07-03 11:39:07.381031"
+
+
+def create(name, contents):
+    with open(os.path.join(*name), "wt") as f:
+        f.write(contents)
+
 
 def server(path):
     os.chdir(path)
-    os.mkdir("domain1")
 
-    open(os.path.join("domain1", "__DUMMY"), "wt").write("something to ignore")
-    open(os.path.join("domain1", "withoutinfo"), "wt").write("without info")
-    open(os.path.join("domain1", "withinfo"), "wt").write("with info")
-    open(os.path.join("domain1", "withinfo.info"), "wt").write('{"datetime": "%s"}' % DATETIMETEST)
+    os.mkdir("domain1")
+    create(("domain1", "__DUMMY"), "something to ignore")
+    create(("domain1", "withoutinfo"), "without info")
+    create(("domain1", "withinfo"), "with info")
+    create(("domain1", "withinfo.info"),
+           '{"datetime": "%s", "tags": "search"}' % DATETIMETEST)
+
+    os.mkdir("comp")
+    with gzip.open(os.path.join("comp", "gz"), "wt") as f:
+        f.write("compress")
+    create(("comp", "gz.info"), '{"compression": "gz"}')
+    with bz2.open(os.path.join("comp", "bz2"), "wt") as f:
+        f.write("compress")
+    create(("comp", "bz2.info"), '{"compression": "bz2"}')
+    create(("intar",), "compress")
+    with tarfile.open(os.path.join("comp", "tar.gz"), "w") as tar:
+        tar.add(os.path.join("intar"))
+    os.remove("intar")
+    create(("comp", "tar.gz.info"), '{"compression": "tar.gz"}')
 
     httpd = HTTPServer(("", 12345),  SimpleHTTPRequestHandler)
     httpd.serve_forever()
@@ -49,12 +71,21 @@ class TestServerFiles(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.path)
 
+    def test_callback(self):
+        class CB:
+            run = 0
+            def __call__(self):
+                self.run += 1
+        cb = CB()
+        self.lf.download("domain1", "withinfo", callback=cb)
+        self.assertEqual(cb.run, 100)
+
     def test_listdir_server(self):
         ldomain = self.sf.listfiles("domain1")
         self.assertEqual(set(ldomain),
                                 set([ ("domain1", "withinfo"), ("domain1", "withoutinfo")]))
         lall = self.sf.listfiles()
-        self.assertGreaterEqual(set(ldomain), set(lall))
+        self.assertGreaterEqual(set(lall), set(ldomain))
 
     def test_download(self):
         self.lf.download("domain1", "withinfo")
@@ -68,13 +99,59 @@ class TestServerFiles(unittest.TestCase):
         slist = self.sf.listfiles("domain1")
         self.assertEqual(set(llist), set(slist))
 
+    def test_compressed(self):
+        self.lf.download("comp", "gz")
+        self.lf.download("comp", "bz2")
+
+        def read(fname):
+            with open(fname, "rt") as f:
+                return f.read()
+
+        self.assertEqual(read(self.lf.localpath("comp", "gz")),
+                         read(self.lf.localpath("comp", "bz2")))
+
+        self.lf.download("comp", "tar.gz")
+        self.assertTrue(os.path.isdir(self.lf.localpath("comp", "tar.gz")))
+        self.assertEqual(read(self.lf.localpath("comp", "tar.gz", "intar")),
+                         read(self.lf.localpath("comp", "bz2")))
+        self.lf.remove("comp", "tar.gz")
+        self.assertFalse(os.path.exists(self.lf.localpath("comp", "tar.gz")))
+        self.assertFalse(os.path.exists(self.lf.localpath("comp", "tar.gz.info")))
+
+    def test_info(self):
+        self.lf.download("domain1", "withinfo")
+        self.lf.download("domain1", "withoutinfo")
+        self.assertEqual(self.lf.info("domain1", "withinfo")["datetime"], DATETIMETEST)
+        self.assertEqual(self.sf.info("domain1", "withinfo")["datetime"], DATETIMETEST)
+        self.assertEqual(self.lf.allinfo()[("domain1", "withinfo")]["datetime"],
+                         DATETIMETEST)
+        self.assertEqual(self.sf.allinfo()[("domain1", "withinfo")]["datetime"],
+                         DATETIMETEST)
+        self.assertEqual(self.sf.allinfo("domain1"), self.lf.allinfo("domain1"))
+
     def test_remove(self):
         lpath = self.lf.localpath_download("domain1", "withoutinfo")
-
         self.assertTrue(os.path.exists(lpath))
         self.assertTrue(os.path.exists(lpath + ".info"))
         self.lf.remove("domain1", "withoutinfo")
         self.assertFalse(os.path.exists(lpath))
         self.assertFalse(os.path.exists(lpath + ".info"))
-
         self.assertRaises(FileNotFoundError, lambda: self.lf.remove("domain1", "wrong file"))
+
+    def test_update(self):
+        self.lf.update_all()
+        self.assertTrue(self.lf.needs_update("domain1", "withinfo"))
+        self.lf.update("domain1", "withinfo")
+        self.assertFalse(self.lf.needs_update("domain1", "withinfo"))
+
+        self.lf.update("domain1", "withoutinfo")
+        self.assertTrue(self.lf.needs_update("domain1", "withoutinfo"))
+        self.lf.update_all()
+
+    def test_search(self):
+        self.lf.download("domain1", "withinfo")
+        self.lf.download("domain1", "withoutinfo")
+        self.assertEqual(self.lf.search("without"), [("domain1", "withoutinfo")])
+        self.assertEqual(len(self.lf.search("domain1")), 2)
+        self.assertEqual(len(self.sf.search("domain1")), 2)
+        self.assertEqual(self.sf.search("search"), [("domain1", "withinfo")])
